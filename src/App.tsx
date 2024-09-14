1cd584cb41d5d5dd4acaf5faa362382d22e4bb92
Antonio Sitoe <antoniositoehl@gmail.com>
2024-09-14 20:05:49 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  function handle() {
+    window.location.href =
+      'https://portal-7okv-git-modi-sdk-release-antonio-sitoes-projects.vercel.app/d4228666-36a2-46c3-8469-dbc537960f84'
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Registo de dados</h1>
+      <div className="card">
+        <button onClick={handle}>Fazer registo</button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/form.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+
+function Form() {
+  const [load, setLoad] = useState(false)
+  const [data, setData] = useState({
+    name: '',
+    document_number: '',
+    birth_date: '',
+    nationality: '',
+    phone_number: '',
+    physical_address: '',
+    card_number: '',
+    issue_date: '',
+  })
+
+  useEffect(() => {
+    setLoad(true)
+    fetch('https://api-storage-tiaw-sigma.vercel.app/sendData')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json)
+        setData(json)
+      })
+      .finally(() => {
+        setLoad(false)
+      })
+  }, [])
+
+  if (load) return <p>carregando...</p>
+  return (
+    <div>
+      <h3>Nome completo</h3>
+      <span>{data.name}</span>
+
+      <h3>Número do documento</h3>
+      <span>{data.document_number}</span>
+
+      <h3>Data de nascimento</h3>
+      <span>{data.birth_date}</span>
+
+      <h3>Nacionalidade</h3>
+      <span>{data.nationality}</span>
+
+      <h3>Número de telefone</h3>
+      <span>{data.phone_number}</span>
+
+      <h3>Endereço</h3>
+      <span>{data.physical_address}</span>
+
+      <h3>Número do cartão</h3>
+      <span>{data.card_number}</span>
+
+      <h3>Data de emissão do documento</h3>
+      <span>{data.issue_date}</span>
+    </div>
+  )
+}
+
+export { Form }
