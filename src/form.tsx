9e72144a74ab6bee3299859fa1e644b265f4312a
Antonio Sitoe <antoniositoehl@gmail.com>
2024-09-15 15:13:35 +0200

    dsd

--- src/form.tsx
+++ src/form.tsx
@@ -19,7 +19,7 @@ function Form() {
       .then((response) => response.json())
       .then((json) => {
         console.log(json)
-        setData(json.data)
+        setData(json?.data)
       })
       .finally(() => {
         setLoad(false)
@@ -30,28 +30,28 @@ function Form() {
   return (
     <div>
       <h3>Nome completo</h3>
-      <span>{data.name}</span>
+      <span>{data?.name}</span>
 
       <h3>Número do documento</h3>
-      <span>{data.document_number}</span>
+      <span>{data?.document_number}</span>
 
       <h3>Data de nascimento</h3>
-      <span>{data.birth_date}</span>
+      <span>{data?.birth_date}</span>
 
       <h3>Nacionalidade</h3>
-      <span>{data.nationality}</span>
+      <span>{data?.nationality}</span>
 
       <h3>Número de telefone</h3>
-      <span>{data.phone_number}</span>
+      <span>{data?.phone_number}</span>
 
       <h3>Endereço</h3>
-      <span>{data.physical_address}</span>
+      <span>{data?.physical_address}</span>
 
       <h3>Número do cartão</h3>
-      <span>{data.card_number}</span>
+      <span>{data?.card_number}</span>
 
       <h3>Data de emissão do documento</h3>
-      <span>{data.issue_date}</span>
+      <span>{data?.issue_date}</span>
     </div>
   )
 }
